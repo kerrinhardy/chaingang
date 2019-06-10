9a552b8bcc1b5d2a0273a8fca80bbad1eb30ba1f
Kerrin Hardy <info@kerrinhardy.com>
2019-06-10 21:52:36 +0930

    First commit

---
+++ private/db_credentials.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kerrin
+ * Date: 9/6/19
+ * Time: 10:35 PM
+ */
+
+// Keep database credentials in a separate file
+// 1. Easy to exclude this file from source code managers
+// 2. Unique credentials on development and production servers
+// 3. Unique credentials if working with multiple developers
+
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "lynda-tutorial-1");
---
+++ public/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kerrin
+ * Date: 9/6/19
+ * Time: 8:10 PM
+ */
+
+require_once('../private/initialise.php'); ?>
+
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+    <div id="main">
+
+        <ul id="menu">
+            <li><a href="<?php echo url_for('/bicycles.php'); ?>">View Our Inventory</a></li>
+            <li><a href="<?php echo url_for('/about.php'); ?>">About Us</a></li>
+        </ul>
+
+    </div>
+
+<?php $super_hero_image = 'AdobeStock_18040381_xlarge.jpeg'; ?>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
